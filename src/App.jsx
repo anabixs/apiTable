7f07e9e6d8aca093dd762e7d4d6ecd02a1749004
anabixs <vladkolumbus@gmail.com>
2023-11-08 01:12:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+
+import Table from "./components/Table";
+import Comments from "./components/Comments";
+
+export default function App() {
+  const [route, setRoute] = useState("/");
+  const [selectedItemId, setSelectedItemId] = useState(null);
+
+  const navigateTo = (newRoute) => {
+    setRoute(newRoute);
+  };
+  const handleItemSelect = (itemId) => {
+    setSelectedItemId(itemId);
+    navigateTo(`/comments/${itemId}`);
+  };
+  return (
+    <>
+      <header className="header">
+        <h1 className="logo"></h1>
+        <ul className="main-nav">
+          <li>
+            <span onClick={() => navigateTo("/news")}>News</span>
+          </li>
+          <li>
+            <span onClick={() => navigateTo("/shows")}>Shows</span>
+          </li>
+          <li>
+            <span onClick={() => navigateTo("/jobs")}>Jobs</span>
+          </li>
+        </ul>
+      </header>
+      <div className="container">
+        {route === "/" && (
+          <Table
+            apiEndpoint="https://api.hnpwa.com/v0/news/1.json"
+            onItemSelect={handleItemSelect}
+          />
+        )}
+        {route === "/news" && (
+          <Table
+            apiEndpoint="https://api.hnpwa.com/v0/news/1.json"
+            onItemSelect={handleItemSelect}
+          />
+        )}
+        {route === "/shows" && (
+          <Table
+            apiEndpoint="https://api.hnpwa.com/v0/show/1.json"
+            onItemSelect={handleItemSelect}
+          />
+        )}
+        {route === "/jobs" && (
+          <Table
+            apiEndpoint="https://api.hnpwa.com/v0/jobs/1.json"
+            onItemSelect={handleItemSelect}
+          />
+        )}
+        {route.startsWith("/comments/") && <Comments props={selectedItemId} />}
+      </div>
+    </>
+  );
+}
---
+++ src/components/Comments.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+export default function Comments({ props }) {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://api.hnpwa.com/v0/item/${props}.json`)
+      .then((response) => {
+        setUser(response.data.comments);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, [props]);
+  console.log(user);
+  return (
+    <>
+      <ol>
+        {user.map((item) => (
+          <li key={item.id}>
+            <span className="usname">{item.user}</span> {item.content}
+          </li>
+        ))}
+      </ol>
+    </>
+  );
+}
